90a826b689aa9d4063cad6c547b4369e90f26f4f
Octavian <51758889+fsty@users.noreply.github.com>
2021-01-11 14:14:16 +0100

    Update main.js

--- main.js
+++ main.js
@@ -23,7 +23,7 @@ client.login(token);
 
 
 /* this program depends on the discord.js NodeJS repo, install it manually by opening your shell terminal and run the following command
-in the specified folder you want to use for the bot, example => "C:/Home/Documents/TheBotGoesHere" 
+in the specified folder you want to use for the bot, example => "C:/User/Documents/TheBotGoesHere" 
 change the directory to said folder and run the command "npm i discord.js" 
 using the discord API: https://www.youtube.com/watch?v=AQvdfRwbq58
 DevCoffee tutorial: https://www.youtube.com/watch?v=TaB2UDdX5Bw - super explanatory.
